c88066453ba6fa5f276a3cc175e545030e258787
stevehuang <xhtxsteve@163.com>
2025-06-08 14:53:21 +0800

    Update RandomNumArr.py

--- 9618paper4/RandomNumArr.py
+++ 9618paper4/RandomNumArr.py
@@ -3,8 +3,10 @@ import random
 
 count = 0
 arr = []
+j = 1000
 
-for i in range(1000):
+
+for i in range(j):
     arr.append(random.randint(1,1000))
     
 for num in arr:
@@ -15,12 +17,20 @@ for num in arr:
         
           
 # bubble sort        
-for j in range(999, 0, -1):
-    for k in range (j):
+while True:
+    swap = False
+    for k in range (j-1):
         if arr[k+1] < arr[k]:
             temp = arr[k+1]
             arr[k+1] = arr[k]
             arr[k] = temp
-        
+            swap = True
+    j -= 1
+    if swap == False:
+        break        
+    
+    
+    
+
 print(arr)
 
