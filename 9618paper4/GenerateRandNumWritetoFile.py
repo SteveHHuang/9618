04777f2cedceeb24d8cf93a7646642cb2bb1a352
SteveH <xhtxsteve@163.com>
2025-06-15 09:18:27 +0800

    20250615 update

--- 9618paper4/GenerateRandNumWritetoFile.py
+++ 9618paper4/GenerateRandNumWritetoFile.py
@@ -3,24 +3,32 @@
 # summation all numbers
 # finding numbers, if found, return true, otherwise return false
 
+import os
 import random
 
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__)) # 获取脚本所在目录的绝对路径
+
 def WritingRandNum(name):
-    with open(name,"w") as n:
+    filepath = os.path.join(SCRIPT_DIR, name) # 拼接txt文件路径
+    with open(filepath,"w") as n:
         for i in range(100):
             n.write(str(random.randint(1,100))+"\n")
     n.close()
     
 def Summation(name):
     sum = 0
-    with open(name, "r") as n:
-        for i in range(100):
-            sum += int(n.readline(5))
+    filepath = os.path.join(SCRIPT_DIR, name)
+
+    with open(filepath, "r") as n:
+        for i in n:
+            sum += int(i.strip())
     n.close()
     return sum
 
 def NumExistence(name,num):
-    with open(name, "r") as n:
+    filepath = os.path.join(SCRIPT_DIR, name)
+
+    with open(filepath, "r") as n:
         for i in range(100):
             temp = int(n.readline(5))
             if temp == num:
