cd45b40081ab73d4ac096ac7642204609860ca62
stevehuang <xhtxsteve@163.com>
2025-06-16 20:19:53 +0800

    Update 20250616LinkedList.py

--- 9618paper4/20250616LinkedList.py
+++ 9618paper4/20250616LinkedList.py
@@ -5,13 +5,15 @@ import random
 #Step1: initialise a 2D array to implement linked list, 10rows, 2 columns
 LinkedList = [[0, i] for i in range(1,11)]
 LinkedList[9][1] = -1
+print(LinkedList)
 # print(LinkedList)
 
-# Initialize headpointer, freeptr
+# Step2: Initialize headpointer, freeptr
 
-headptr = -1 # headptr always 
+headptr = -1 # headptr always point to the address of the first used node
 freeptr = 0 # freeptr always point to the first free space
 
+# Step3: Adding value
 def addData(val):
     global headptr, freeptr, LinkedList
     if freeptr == -1:
@@ -21,22 +23,24 @@ def addData(val):
         headptr = freeptr
         freeptr = LinkedList[freeptr][1] # freeptr always point to the first free space
         LinkedList[headptr][0] = val
-        LinkedList[headptr][1] = -1 # 添加第一个数据的时候 第一个node的ptr是-1
+        LinkedList[headptr][1] = -1 # 添加第一个数据的时候 因为后面没有数据 所以第一个node的ptr是-1
         return "successfully added"
         
-    PlaceToAdd = headptr
-    while LinkedList[PlaceToAdd][1] != -1:
-        PlaceToAdd = LinkedList[PlaceToAdd][1]
+    nextPlaceToAdd = headptr
+    while LinkedList[nextPlaceToAdd][1] != -1:
+        nextPlaceToAdd = LinkedList[nextPlaceToAdd][1]
     
     temp = freeptr # address of the new node
     freeptr = LinkedList[freeptr][1]
-    LinkedList[PlaceToAdd][1] = temp
+    LinkedList[nextPlaceToAdd][1] = temp # -1 to temp
     LinkedList[temp][0] = val
     LinkedList[temp][1] = -1
     
 
 for i in range(10):         
-    addData(random.randint(1,1000))
+    # addData(random.randint(1,1000))
+    addData(19)
+    print(LinkedList)
    
    
 def searchNum(num, arr):  
@@ -46,6 +50,6 @@ def searchNum(num, arr):
         return -1
 
 
-print(LinkedList)    
-print(searchNum(228, LinkedList))
+# print(LinkedList)    
+# print(searchNum(228, LinkedList))
    
