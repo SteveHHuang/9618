3d736ca6ff4bac4129e88314f2b0af44ff81ed40
SteveH <xhtxsteve@163.com>
2025-06-17 13:30:29 +0800

    erroneous ver.

--- 9618paper4/ADT_Implementation/20250617LinkedListSorting.py
+++ 9618paper4/ADT_Implementation/20250617LinkedListSorting.py
@@ -12,7 +12,8 @@ headptr = -1 # headptr always point to the address of the first used node
 freeptr = 0 # freeptr always point to the address of first free space
 
 # Step3: Adding value
-def addDatainSeq(val):
+#Descendign order
+def addDatainSeq(val): 
     global headptr, freeptr, LinkedList
     if freeptr == -1:
         return "no space"
