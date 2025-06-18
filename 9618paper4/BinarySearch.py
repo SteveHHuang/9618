5385824a6f842897a145dc3462aa7a67539bff9c
stevehuang <xhtxsteve@163.com>
2025-06-18 14:28:54 +0800

    20250618 update

---
+++ 9618paper4/BinarySearch.py
@@ -0,0 +1,24 @@
+# 20250618 binarysearch
+
+arr = []
+
+for i in range(100):
+    arr.append(i+1)
+
+def binarysearch(num):
+    global arr
+    lower = 0
+    upper = len(arr)-1
+    
+    while lower <= upper:
+        mid = int((lower+upper)/2)
+        print(lower,upper, mid)
+        if arr[mid] == num:
+            return True
+        elif arr[mid] > num:
+            upper = mid - 1
+        elif arr[mid] < num:
+            lower = mid + 1    
+    return False
+
+print(binarysearch(2))        
