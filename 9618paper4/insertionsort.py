8f634d2728b380da6d857093a1cda02e7c81a439
SteveH <xhtxsteve@gmail.com>
2025-08-29 20:15:22 +0800

    insertion sort from 9608/42 s18

--- 9618paper4/insertionsort.py
+++ 9618paper4/insertionsort.py
@@ -14,3 +14,21 @@ def insertion_sort(arr):
 insertion_sort(arr)
 print(arr)       
      
+# ALT version of insertion sort
+# modified from 9608/42/M/J/18 Q3b
+brr = [4,3,5,7,2]
+def InsertionSort(arr):
+    for i in range(1, len(arr)):
+        HolePosition = i
+        temp = arr[i]
+        while (HolePosition > 0 and arr[HolePosition-1]>temp):
+            arr[HolePosition] = arr[HolePosition-1]
+            HolePosition -= 1
+            print(arr)
+            
+        arr[HolePosition] = temp
+        print(arr)
+        
+    
+    
+InsertionSort(brr)
